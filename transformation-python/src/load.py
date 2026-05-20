05ae8988b5479257aee966369110845889fcc451
Leandro Fratel de Souza <leandrofratel@gmail.com>
2026-05-19 22:24:37 -0300

    Sprint 3: Tabela fato criada, inicio da criação do DW

---
+++ transformation-python/src/load.py
@@ -0,0 +1,43 @@
+import json
+import pandas as pd
+
+from pathlib import Path
+
+
+CAMINHO_RAW = Path("../data/raw/characters")
+CAMINHO_GOLD = Path("../data/gold")
+
+
+def criar_fact_character_episode():
+    registros = []
+    arquivos = sorted(CAMINHO_RAW.glob("*.json"))
+
+    # printa a quantidade de arquivos
+    print(f"{len(arquivos)}")
+
+    for arquivo in arquivos:
+        print(f"Lendo: {arquivo.name}")
+        with open(arquivo, "r", encoding="utf-8") as f:
+            dados = json.load(f)
+
+        for personagem in dados["results"]:
+            character_id = personagem["id"]
+
+            for url in personagem["episode"]:
+                episode_id = int(url.split("/")[-1])
+                registros.append(
+                    {
+                        "character_id": character_id,
+                        "episode_id": episode_id
+                    }
+                )
+    df = pd.DataFrame(registros)
+
+    arquivo_saida = (CAMINHO_GOLD / "fact_character_episode.parquet")
+    df.to_parquet(arquivo_saida, index=False)
+
+    print(f"Arquivo salvo em: {arquivo_saida}")
+
+
+if __name__=="__main__":
+    criar_fact_character_episode()
