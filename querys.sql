7a96d339f72608b80e5497c34defda19987e5814
Leandro Fratel de Souza <leandrofratel@gmail.com>
2026-06-17 14:16:24 -0300

    Remove arquivos indesejados do repositório e atualiza .gitignore

--- querys.sql
+++
@@ -1,9 +0,0 @@
-// Querys
-
-// Identifica a quantidade de personagens por episodio.
-SELECT
-	episode_id,
-	COUNT(character_id) as total_personagens
-FROM DATA
-GROUP BY episode_id
-ORDER BY total_personagens DESC
